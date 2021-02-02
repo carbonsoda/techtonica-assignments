0d778dd3cac7caa83ccb6ca8dbe6bc8e078a4d03
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-02-02 15:31:25 -0800

    Add Quesiton 6 note

--- exercises/3_basic-js-practice.js
+++ exercises/3_basic-js-practice.js
@@ -52,7 +52,7 @@ function factorial(n) {
 /* [Challenge 6] Switch Statements
 Using a switch statement, write a function called sleep_in(weekday, vacation) with these criteria: The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 */
-
+// TODO: clarify weekday parameter
 
 /* [Challenge 7] Accessing Arrays
 Write a function called common(a, b) with these criteria: 
